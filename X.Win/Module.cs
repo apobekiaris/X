6cca4e9b0c549fec9a8b8b9018ac1e75b3ea5eee
apobekiaris <apostolis.bekiaris@gmail.com>
2012-07-16 00:36:13 +0300

    add a new empty windows defendant core module X.Win and put both under the same folder

---
+++ X.Win/Module.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+
+namespace X.Win {
+    public sealed partial class XWinModule : ModuleBase {
+        public XWinModule() {
+            InitializeComponent();
+        }
+    }
+}
