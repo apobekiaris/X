6b62705d2337bca8ec15a85d075bbbea12222b28
apobekiaris <apostolis.bekiaris@gmail.com>
2012-07-15 04:27:10 +0300

    E2829-How to generate and assign a sequential number for a business object within a database transaction, while being a part of a successful saving process (XAF)
    http://www.devexpress.com/Support/Center/e/E2829.aspx

--- DatabaseUpdate/Updater.cs
+++ DatabaseUpdate/Updater.cs
@@ -1,16 +1,16 @@
 using System;
-using System.Security.Principal;
-
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Updating;
-using DevExpress.Xpo;
-using DevExpress.Data.Filtering;
+using X.Sequence;
 
 namespace X.DatabaseUpdate {
     public class Updater : ModuleUpdater {
         public Updater(IObjectSpace objectSpace, Version currentDBVersion) : base(objectSpace, currentDBVersion) { }
         public override void UpdateDatabaseAfterUpdateSchema() {
             base.UpdateDatabaseAfterUpdateSchema();
+            SequenceGeneratorInitializer.Initialize();
+            //Dennis: It is necessary to register sequences for persistent types used in your application.
+            SequenceGenerator.RegisterSequences(XafTypesInfo.Instance.PersistentTypes);
         }
     }
 }
--- Module.cs
+++ Module.cs
@@ -1,12 +1,15 @@
-using System;
-using System.Collections.Generic;
-
 using DevExpress.ExpressApp;
+using X.Sequence;
 
 namespace X {
     public sealed partial class XModule : ModuleBase {
         public XModule() {
             InitializeComponent();
+        }
+
+        public override void Setup(XafApplication application) {
+            base.Setup(application);
+            SequenceGeneratorInitializer.Register(application);
         }
     }
 }
