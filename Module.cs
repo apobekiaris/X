3d87e6986205d2f290b951805c2855290d11350e
apobekiaris <apostolis.bekiaris@gmail.com>
2012-07-15 04:20:43 +0300

    New empty XAF module

---
+++ Module.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+
+namespace X {
+    public sealed partial class XModule : ModuleBase {
+        public XModule() {
+            InitializeComponent();
+        }
+    }
+}
